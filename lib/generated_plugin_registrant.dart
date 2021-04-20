b27c51ebd4b3ea15a402d4b3be23516ec7c7674b
yoojh9 <yoojh9@gmail.com>
2021-04-20 14:10:08 +0900

    geocoder -> geocoding 라이브러리 변경

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,7 +5,6 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:geolocator_web/geolocator_web.dart';
 import 'package:location_web/location_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -14,7 +13,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FirebaseCoreWeb.registerWith(registrar);
-  GeolocatorPlugin.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
